76fbd724e4a785471f57b2856c88b86153de1dca
Tatsuki Hashimoto <hashimoto19980924@gmail.com>
2020-11-28 23:03:34 +0900

    bump

--- packages/celer-types/src/interfaces/augment-api-tx.js
+++ packages/celer-types/src/interfaces/augment-api-tx.js
@@ -1,48 +1,4 @@
 "use strict";
 // Auto-generated via `yarn polkadot-types-from-chain`, do not edit
 /* eslint-disable */
-{
-    Bytes, Compact, Option, Vec, u128, u32, u64, u8;
-}
-from;
-'@polkadot/types';
-{
-    AnyNumber;
-}
-from;
-'@polkadot/types/types';
-{
-    CodeHash, Gas, Schedule;
-}
-from;
-'@polkadot/types/interfaces/contracts';
-{
-    Extrinsic;
-}
-from;
-'@polkadot/types/interfaces/extrinsics';
-{
-    GrandpaEquivocationProof, KeyOwnerProof;
-}
-from;
-'@polkadot/types/interfaces/grandpa';
-{
-    AccountId, AccountIndex, Address, Balance, BalanceOf, BlockNumber, Call, ChangesTrieConfiguration, Hash, KeyValue, LookupSource, Moment, Perbill, Weight;
-}
-from;
-'@polkadot/types/interfaces/runtime';
-{
-    Key;
-}
-from;
-'@polkadot/types/interfaces/system';
-{
-    CooperativeSettleRequestOf, CooperativeWithdrawRequestOf, OpenChannelRequestOf, PayIdList, ResolvePaymentConditionsRequestOf, SignedSimplexStateArrayOf, VouchedCondPayResultOf;
-}
-from;
-'celer-types/interfaces/celerPayModule';
-{
-    ApiTypes, SubmittableExtrinsic;
-}
-from;
-'@polkadot/api/types';
+Object.defineProperty(exports, "__esModule", { value: true });
--- packages/celer-types/src/interfaces/celerPayModule/types.js
+++ packages/celer-types/src/interfaces/celerPayModule/types.js
@@ -2,18 +2,3 @@
 // Auto-generated via `yarn polkadot-types-from-defs`, do not edit
 /* eslint-disable */
 Object.defineProperty(exports, "__esModule", { value: true });
-{
-    Bytes, Enum, Option, Set, Struct, Vec, bool, u128, u32, u64, u8;
-}
-from;
-'@polkadot/types';
-{
-    Signature;
-}
-from;
-'@polkadot/types/interfaces/extrinsics';
-{
-    AccountId, Balance, BlockNumber, Call, Hash, Moment;
-}
-from;
-'@polkadot/types/interfaces/runtime';
